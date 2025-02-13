04923e9928905a62864df40bbf1b102c29dd031c
sakshi-dahiya27 <sakshidahiya2709@gmail.com>
2025-02-13 10:36:59 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    public static void main(String[] args) {
+        System.out.println("NAME\tMAIL-ID\t\tEMPLOYEE-CODE\tPHONE");
+        System.out.println("Parul\tparil@cse\tp09987\t\t887899090");
+        System.out.println("Sakshi\tsakshi@cse\tp09887\t\t88782090");
+        System.out.println("Gauri\tgori@cse\tp09965\t\t887677790");
+        System.out.println("Kiran\tkiran@cse\tp09987\t\t887755090");
+        System.out.println("Hiran\thiran@cse\tp09961\t\t887611090");
+
+    }
+}
