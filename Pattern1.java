09307d0455480436f8bb4d021a2cf800c23923ad
sakshi-dahiya27 <sakshidahiya2709@gmail.com>
2025-02-20 12:53:11 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,11 @@
+public class Pattern1 {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
