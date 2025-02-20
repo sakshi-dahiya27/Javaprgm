22acd0e6555561a52339367a2c8614a54316e669
sakshi-dahiya27 <sakshidahiya2709@gmail.com>
2025-02-20 12:53:43 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,11 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        int i, j;
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                System.out.print(i+j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
