0fa2048b50ccbd7b32753d1396ae672d0cb75932
sakshi-dahiya27 <sakshidahiya2709@gmail.com>
2025-02-21 11:46:22 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,8 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = { 5, 3, 7, 4, 5 };
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
